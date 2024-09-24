3b501f3b27bfced4f589b2ad6afb957b50b6902f
Kelvin Ward <kelvinward1010@gmail.com>
2024-09-24 16:57:33 +0700

    update pages

--- src/pages/about/views/About.tsx
+++ src/pages/about/views/About.tsx
@@ -8,8 +8,27 @@ export function About() {
             <TitlePage title="About" description="Introduce myself" />
             <div className={styles.top}>
                 <img className={styles.imgavt} src={ImgAVT} alt="avt" />
+                <p className="text-normal">Tyler Locke</p>
                 <p className="text-normal">I'm a web developer</p>
             </div>
+            <br />
+            <p className="text-normal">
+                Hello, my name is Tyler Locke. I am a student at Vietnam
+                National University of Agriculture, Faculty of Information
+                Technology. I aim to become a FullStack programmer in the
+                future.
+            </p>
+            <br />
+            <p className="text-normal">
+                I was born and raised in my beloved hometown of Hai Duong until
+                I was 17 years old when I moved to Hanoi to study at university.
+                I am currently living in Hanoi to work and study. My hobbies are
+                watching movies, playing games, coding, traveling, and cooking.
+                I like to spend time learning English, and coding is the main
+                thing. I like to talk and share different stories with others. I
+                don't like to drink a lot of alcohol because it is harmful to
+                the body; especially don't use stimulants.
+            </p>
         </div>
     );
 }
--- src/pages/home/views/Home.tsx
+++ src/pages/home/views/Home.tsx
@@ -6,7 +6,7 @@ export function Home() {
         <div className={styles.container}>
             <TitlePage title="Home" description="Youtuber / Sofware Engineer" />
             <p className="text-normal">
-                Hello, my name is Dao Van Duy. I am a student at Vietnam
+                Hello, my name is Tyler Locke. I am a student at Vietnam
                 National University of Agriculture, Faculty of Information
                 Technology. I aim to become a FullStack programmer in the
                 future.
