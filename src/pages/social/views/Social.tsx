d2aa5188068ea8617f41fc4b20aa33f1a2634362
Kelvin Ward <kelvinward1010@gmail.com>
2024-10-08 15:46:02 +0700

    update social conteny

--- src/pages/social/views/Social.tsx
+++ src/pages/social/views/Social.tsx
@@ -1,15 +1,55 @@
 import TitlePage from "@/components/titlepage/TitlePage";
 import styles from "./Social.module.scss";
 import SocialChildren from "../components/SocialChildren";
+import { ImgAVT } from "@/assets/imgs";
 
 export function Social() {
+    function FormFaceBook() {
+        return (
+            <div className={styles.socialContainer}>
+                <div className={styles.imgSocial}>
+                    <img src={ImgAVT} alt="img-logo" />
+                </div>
+                <div className={styles.moreSocial}>
+                    <button>See more</button>
+                </div>
+            </div>
+        );
+    }
+
+    function FormInstagram() {
+        return (
+            <div className={styles.socialContainer}>
+                <div className={styles.imgSocial}>
+                    <img src={ImgAVT} alt="img-logo" />
+                </div>
+                <div className={styles.moreSocial}>
+                    <button>See more</button>
+                </div>
+            </div>
+        );
+    }
+
+    function FormLinkedIn() {
+        return (
+            <div className={styles.socialContainer}>
+                <div className={styles.imgSocial}>
+                    <img src={ImgAVT} alt="img-logo" />
+                </div>
+                <div className={styles.moreSocial}>
+                    <button>See more</button>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className={styles.container}>
             <TitlePage title="Social Media" description="Social Media" />
             <div>
-                <SocialChildren lable="Facebook" content="okkk" />
-                <SocialChildren lable="Instagram" content="okkk" />
-                <SocialChildren lable="LinkedIn" content="okkk" />
+                <SocialChildren lable="Facebook" content={<FormFaceBook />} />
+                <SocialChildren lable="Instagram" content={<FormInstagram />} />
+                <SocialChildren lable="LinkedIn" content={<FormLinkedIn />} />
             </div>
         </div>
     );
