76351e668df4d81ae0179e4da80393b8c7138f9d
Kelvin Ward <kelvinward1010@gmail.com>
2024-10-15 13:56:46 +0700

    update project

--- src/pages/project/components/projectCpt/ProjectCpt.tsx
+++ src/pages/project/components/projectCpt/ProjectCpt.tsx
@@ -1,25 +1,39 @@
 import useThemeMode from "@/hooks/useThemeMode";
 import styles from "./ProjectCpt.module.scss";
 import BoxLanguages from "../BoxLanguages";
+import { IconArrowUpRight } from "@tabler/icons-react";
 
 function ProjectCpt() {
     const { isDarkMode } = useThemeMode();
+    const handleButtonClick = (url: string) => {
+        window.open(url, "_blank");
+    };
     return (
         <div
             className={`${styles.container} ${isDarkMode ? styles.themeDark : styles.themeLight}`}
         >
             <div className={styles.titlebox}>
                 <h1 className={`${styles.title} text-normal`}>Project Title</h1>
-                <span className="text-normal">stars: 5</span>
             </div>
             <p className={`${styles.des} text-normal`}>
                 This is a description project
             </p>
-            <p className="text-normal">Update at 7:00 - 10/10/2024</p>
             <div className={styles.languages}>
                 <BoxLanguages title="JavaScript" />
                 <BoxLanguages title="TypeScript" />
                 <BoxLanguages title="Python" />
+            </div>
+            <div className={styles.boxbutton}>
+                <button
+                    onClick={() =>
+                        handleButtonClick("https://github.com/kelvinward1010")
+                    }
+                >
+                    <div>
+                        On github <IconArrowUpRight size={15} />
+                    </div>
+                </button>
+                <button>See more</button>
             </div>
         </div>
     );
